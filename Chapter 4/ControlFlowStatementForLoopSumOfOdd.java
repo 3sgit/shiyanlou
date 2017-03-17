e913ceddd77391ba8590fc139c6ac7dc7b044899
3sgit <3sidea_reg@outlook.com>
2017-03-17 16:12:47 +0800

    Update ControlFlowStatementForLoopSumOfOdd.java

--- Chapter 4/ControlFlowStatementForLoopSumOfOdd.java
+++ Chapter 4/ControlFlowStatementForLoopSumOfOdd.java
@@ -1,6 +1,6 @@
 // 2.2.1 SumOfOdd
 public class ControlFlowStatementForLoopSumOfOdd {
-    public static void main (String [] args) {
+    public static void main (String[] args) {
         
         int sum = 0;
         for (int i=1; i<=1000; i++) {
