5c48fe6058496bf9aaed0865e3d9a6ae9b94872e
3sgit <3sidea_reg@outlook.com>
2017-03-18 11:28:38 +0800

    Create chapter5ArraysExample2.java

---
+++ Chapter 5 Arrays/chapter5ArraysExample2.java
@@ -0,0 +1,14 @@
+/* Chapter 5 Arrays
+// Exmaple 2
+// Two-dimensonal arrays
+*/
+
+public class chatper5ArraysExample {
+  public static void main(String[] args) {       
+    String[][] name = { {"ZhaoYi", "QianEr", "SunSan"}, {"LiSi", "ZhouWu", "WuLiu"} };       
+      for(int=i) {
+      
+      }       
+     }
+  }
+}
