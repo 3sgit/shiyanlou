b950b259d4514efde9467cc72007f59c3d0723c8
3sgit <3sidea_reg@outlook.com>
2017-03-21 17:14:50 +0800

    Update chapter6MethodAssignment.java

--- Chapter 6 Method/chapter6MethodAssignment.java
+++ Chapter 6 Method/chapter6MethodAssignment.java
@@ -3,6 +3,23 @@
 
 public class chapter6MethodAssignment {
   public static void main (String[] args) {
-    
+    int n = 5;
+    int count = 0;
+    for (int i=0; i<=n; i++) { 
+      for (int j=0; j<i; j++) {
+      ++count;
+      System.out.printf("%02d ", count); 
+      }
+    System.out.println();
+    }
   }
 }
+
+/*
+Print out:
+01 
+02 03 
+04 05 06 
+07 08 09 10 
+11 12 13 14 15 
+*/
