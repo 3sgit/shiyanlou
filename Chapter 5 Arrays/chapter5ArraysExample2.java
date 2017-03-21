d3f63dfb4041e5231d8ff0dde1da4138c2290357
3sgit <3sidea_reg@outlook.com>
2017-03-21 14:03:44 +0800

    Update chapter5ArraysExample2.java

--- Chapter 5 Arrays/chapter5ArraysExample2.java
+++ Chapter 5 Arrays/chapter5ArraysExample2.java
@@ -6,8 +6,10 @@
 public class chatper5ArraysExample {
   public static void main (String[] args) {       
     String[][] name = { {"ZhaoYi", "QianEr", "SunSan"}, {"LiSi", "ZhouWu", "WuLiu"} };       
-      for (int i; ) {
-        //... save point
+      for (int i = 0; i<name.length; i++) {
+        for (int j = 0; j < name[i].length; j++) {
+          System.out.println (name[i][j]);
+        }
       }       
      }
   }
