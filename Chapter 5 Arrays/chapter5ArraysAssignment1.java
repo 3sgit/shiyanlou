3c10871dc4b662e059cec3cce13d4c693e7bc939
3sgit <3sidea_reg@outlook.com>
2017-03-21 15:10:41 +0800

    Create chapter5ArraysAssignment1.java

---
+++ Chapter 5 Arrays/chapter5ArraysAssignment1.java
@@ -0,0 +1,19 @@
+# chapter 5 Arrays assignment
+# To print from 1 to 100 by using Arrays
+
+public class chapterArraysAssignment1 {
+  public static void main (String[] args) {
+    int num[][] = new int[10][10];
+    int count = 0;
+      for (int i=0; i<num.length; i++) {
+        for (int j=0; j<num[i].length; j++) {
+          ++count;
+        //System.out.printf("%d%s", num[i][j], "\t");
+          System.out.printf("%3d", num[i][j]);
+          }
+          System.out.println();  
+        }
+      }
+    
+  }
+}
