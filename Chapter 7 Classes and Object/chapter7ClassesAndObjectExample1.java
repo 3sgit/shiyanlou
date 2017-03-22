5f0fe8e0af1687bfbeaf4a24f642f87f9e2960a1
3sgit <3sidea_reg@outlook.com>
2017-03-22 20:31:01 +0800

    Update chapter7ClassesAndObjectExample1.java

--- Chapter 7 Classes and Object/chapter7ClassesAndObjectExample1.java
+++ Chapter 7 Classes and Object/chapter7ClassesAndObjectExample1.java
@@ -2,3 +2,28 @@
 // title 2.2
 */
 
+public class People {
+  double height;
+  int age;
+  int sex;
+  
+  void cry () {
+    System.out.println("i'm crying.");
+  }
+  
+  void laugh () {
+    System.out.println("i'm laughing.");
+  }
+  
+  void printMeInfo () {
+    System.out.println("my height is " + this.height + " ." + "\n"
+                       + "i'm " + this.age + " .");
+    
+    if () {
+      System.out.println("i am a man."); 
+    }
+    else {
+      System.out.println("i am a female."); 
+    }
+  }
+}
