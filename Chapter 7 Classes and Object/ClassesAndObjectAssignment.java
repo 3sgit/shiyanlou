9d7bdca66f1894a8eb89238824b28d7031dc77de
3sgit <3sidea_reg@outlook.com>
2017-03-23 14:40:43 +0800

    Create ClassesAndObjectAssignment.java

---
+++ Chapter 7 Classes and Object/ClassesAndObjectAssignment.java
@@ -0,0 +1,15 @@
+/*  chapter 7 classes and object
+//  assignment
+//  
+*/
+
+public class Computer{
+  String machineType;
+  //ddmmyyyy
+  int billDate = 00001990;
+  String serialNumber;
+  
+  public static void main (String[] args) {
+    
+  }
+}
