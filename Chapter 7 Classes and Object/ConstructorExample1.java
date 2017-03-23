302b13d9c00cbafae9b62f14a7e4cad896199d1f
3sgit <3sidea_reg@outlook.com>
2017-03-23 13:17:32 +0800

    Update ConstructorExample1.java

--- Chapter 7 Classes and Object/ConstructorExample1.java
+++ Chapter 7 Classes and Object/ConstructorExample1.java
@@ -2,6 +2,15 @@
 // here is an example of constructor in JAVA.
 */
 
-public class ConstructorExample1{
-
+public class ConstructorExample1 {
+  //People
+  int age;
+  int sex;
+  String name;
+  
+  ConstructorExample (int age, int sex, String name) {
+    this.age = age;
+    this.sex = sex;
+    this.name = name;
+  }
 }
