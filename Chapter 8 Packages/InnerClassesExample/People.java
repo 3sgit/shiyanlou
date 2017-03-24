9e33a044c2d7f0c59b574438366d229829286c66
3sgit <3sidea_reg@outlook.com>
2017-03-24 13:13:30 +0800

    Create People.java

---
+++ Chapter 8 Packages/InnerClassesExample/People.java
@@ -0,0 +1,20 @@
+/* chapter 8 packages 2.5.2 example
+// static nested class vs non-static nested class 
+// http://stackoverflow.com/questions/1353309/java-static-vs-non-static-inner-class
+*/
+package InnerClassesExample;
+
+public class People {[
+  
+  class Student {
+  }
+  
+  static class Teacher {
+
+  }
+  
+  public static void main (String[] args) {
+    
+  }
+}
+
