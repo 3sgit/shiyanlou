d2f97af644aace8872de3a916eb795caa265ef30
3sgit <3sidea_reg@outlook.com>
2017-03-25 12:31:27 +0800

    Update Food.java

--- Chapter 8 Packages/InnerClassesExample/Food.java
+++ Chapter 8 Packages/InnerClassesExample/Food.java
@@ -1,26 +1,31 @@
-/*  chapter 8 packages 2.5.1 exmaple
+/*  chapter 8 packages 2.5.1 example
 //  inner class
 */
-package InnerClassesExample;
+
 
 //Outer class
 public class Food { 
-  private String name = "Outer";
+   private String name = "Outer";
   
  //Inner class
     public class Apple {
       String name = "Inner";
 
     void stuInfo () {
-      System.out.println("Access variable "name" in outer class: " + super.name);
-      System.out.println("Access variable "name" in inner class: " + this.name);
+      System.out.println("Access variable \"name\" in outer class: " + new Food().name);
+      System.out.println("Access variable \"name\" in inner class: " + this.name);
     }
    }
     
   //test inner class object  
   public static void main (String[] args) {
+    
     Food outer = new Food();
-    Apple inner = new Apple();
+    
+    /* Use outer class to new a inner class object
+    // [X] Apple inner = new Apple();
+    */
+    Apple inner = outer.new Apple();
     
     inner.stuInfo();
   }
